3ada9958edc2abe1b7de5786239b639d1e99cce2
vinod.vms <vinod.sebastian@rediffmail.com>
2020-11-08 17:02:46 +0530

    css changes

--- pages/us.js
+++ pages/us.js
@@ -75,7 +75,7 @@ export async function getServerSideProps() {
   
     const aKey = process.env.API_KEY;
   var url = 'https://newsapi.org/v2/top-headlines?' +
-  'country=us&' + aKey;
+  'country=us&' + 'apiKey=fe9339f8c8e7402ea579273f9d8d0114';
   
 var req = new Request(url);
   const res = await fetch(req)
